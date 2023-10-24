f5c78a9a6d93668f363f076a18a3a0456e8562a6
Isaac-victor <vgandse@gmail.com>
2023-10-24 11:14:57 -0700

    done

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,22 @@
 #include "lists.h"
 
 /**
- * print_listint - this will prints all the elements of all linked list
- * @h: this is the head of the linked list of type listint_t to print
- *
- * Return: this will return the number of nodes
+ * print_listint - prints all elements of a list
+ * @h: list
+ * Return: number of nodes
  */
+
 size_t print_listint(const listint_t *h)
 {
-	size_t con = 0;
+	size_t num_nodes = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		printf("%d\n", h->n);
+		printf("%d\n", h->n); /* autofills 0 if no int value given */
+
+		num_nodes += 1;
 		h = h->next;
-		con++;
 	}
 
-	return (con);
-}
+	return (num_nodes);
+}}
--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 
 /**
- * listint_len - this will be returning the number
- * of elements in a linked lists
- * @h: this is the head pointer linked list of type listint_t to traverse
- *
- * Return: the number of the nodes
+ * listint_len - returns the number of elements in a linked listint_t list
+ * @h: list
+ * Return: number of nodes in list
  */
+
 size_t listint_len(const listint_t *h)
 {
-	size_t con = 0;
+	size_t num_nodes = 0;
 
-	while (h)
+	while (h != NULL)
 	{
+		num_nodes += 1;
 		h = h->next;
-		con++;
 	}
-	return (con);
+
+	return (num_nodes);
 }
